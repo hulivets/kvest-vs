e17cd12e5c52f3c814ea8691613da89c0dfa864e
Hulivets Rostyslav <hulivets.rostislav@gmail.com>
2021-06-04 22:18:46 +0300

    init commit

---
+++ deploy.bash
@@ -0,0 +1,26 @@
+#!/usr/bin bash
+
+#User credential
+LOGIN="zi407998"
+HOST="zi407998.ftp.tools"
+PASS="Kolobok@11"
+PORT=22
+ADDRESS="https://www.eco-help-disinsection.online"
+
+PROJECT_DIRECTORY="eco-help-disinsection.online"
+BUILD_DIRECTORY="dist"
+
+#Build
+npm run build
+
+#sshpass -p $PASS 
+ssh -t $LOGIN@$HOST -p $PORT << OEF
+    #Remove previous build
+    rm -rf ~/$PROJECT_DIRECTORY/www/*
+    echo "Previous build was removed"
+OEF
+
+#Upload build
+#sshpass -p $PASS  
+rsync -a --progress -rp ./$BUILD_DIRECTORY/* $LOGIN@$HOST:~/$PROJECT_DIRECTORY/www
+echo $ADDRESS
