a936d2d05ca955dcb3bcb311319fed5cbc496e9f
Hulivets Rostyslav <hulivets.rostislav@gmail.com>
2021-06-06 03:44:46 +0300

    Mobile and desktop version

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { getChart } from './charts';
+
+import '../assets/styles/reset.scss';
+import '../assets/styles/styles.scss';
+import '../assets/styles/media-queries.scss';
+
+// getChart('Chart-1', [28, 10, 7, 12]);
+// getChart('Chart-2', [44.32, 6, 7, 6]);
+// getChart('Chart-3', [39, 1.2, 10, 20]);
+// getChart('Chart-4', [48, 1.2, 11, 7]);
+// getChart('Chart-5', [13, 3, 12, 41]);
