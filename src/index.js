35e0ef7317bb6ddc911092fdd67d07eafb57e0ca
Hulivets Rostyslav <hulivets.rostislav@gmail.com>
2021-06-06 17:09:01 +0300

    Select

--- src/index.js
+++ src/index.js
@@ -1,11 +1,186 @@
-import { getChart } from './charts';
+import smoothscroll from 'smoothscroll-polyfill';
+import Viewer from 'viewerjs';
+import EmailJS from 'emailjs-com';
+import FormValidator from './validator';
+import Inputmask from 'inputmask';
+
 
 import '../assets/styles/reset.scss';
 import '../assets/styles/styles.scss';
 import '../assets/styles/media-queries.scss';
+import '../assets/styles/viewer.css';
 
-// getChart('Chart-1', [28, 10, 7, 12]);
-// getChart('Chart-2', [44.32, 6, 7, 6]);
-// getChart('Chart-3', [39, 1.2, 10, 20]);
-// getChart('Chart-4', [48, 1.2, 11, 7]);
-// getChart('Chart-5', [13, 3, 12, 41]);
+const galleryIds = {
+    // img id : id gallery
+    'makukha-podsolnukh': 'makukha-podsolnukh-gallery',
+    'makukha-soya': 'makukha-soya-gallery',
+    'shrot-podsolnukh': 'shrot-podsolnukh-gallery',
+    'shrot-soya': 'shrot-soya-gallery',
+    'obolonka-soya': 'obolonka-soya-gallery'
+}
+
+const imgIds = Object.keys(galleryIds);
+const galleries = Object.values(galleryIds);
+
+imgIds.forEach((key, index) => {
+    const currentGallery = new Viewer(document.getElementById(galleries[index]), {
+        zoomable: false,
+        title: false,
+        movable: false,
+        toolbar : {
+            flipHorizontal: false,
+            flipVertical: false,
+            play: false,
+            reset: false,
+            prev: true,
+            next: true,
+        }
+    });
+    document.getElementById(key).addEventListener('click', () => {
+        currentGallery.show(false);
+      })
+});
+
+const credentials = {
+	userId: 'user_9F6kvtX4Y5aPAp1bGZW4A',
+	emailTemplateId: 'template_gkx0yjf',
+	serviceId: 'service_6skoivn'
+};
+
+const menuBtn = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-list');
+const navLinkItems = document.querySelectorAll('.nav-list__link');
+const logo = document.querySelector('.logo');
+const scrollBtn = document.querySelector('.btn-header');
+const form = document.querySelector('form');
+const nameField = document.querySelector('#name');
+const telField = document.querySelector('#tel');
+const commentsField = document.querySelector('#comments');
+const inputs = [nameField, telField];
+const loaderModal  = document.querySelector('.loader-modal');
+const successModal = document.querySelector('.success-modal');
+const navBar = document.querySelector('.navbar');
+const actionButtons = document.querySelector('.action-buttons');
+
+let isMenuOpen = false;
+let isStickyNavbar = false;
+
+EmailJS.init(credentials.userId);
+Inputmask({"mask": "+38(999) 999-99-99"}).mask(telField)
+
+var validator = new FormValidator({
+	alerts : true,
+	events : 'input',
+	regex : {
+		phone        : /^[+]?[0-9]{2}?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{2}[-\s\.][0-9]{2}$/g
+	},
+	classes : {
+		item  : 'field',
+		alert : 'alert',
+		bad   : 'bad'
+	}
+});
+
+function toggleModal(e) {
+	navLinks.classList.toggle('open');
+	menuBtn.classList.toggle('focus');
+	isMenuOpen = !isMenuOpen;
+
+	if (e) {
+		const value = e.target.getAttribute('data-link');
+		const posY = document.querySelector(`#${value}`)
+			.getBoundingClientRect().top + window.pageYOffset - 100;
+		window.scrollTo({
+			behavior: 'smooth',
+			top: posY
+		})
+		
+	}
+}
+
+function sendEmail(name, phone, commentsName) {
+	const {serviceId, emailTemplateId} = credentials;
+	const templateParams = {name, phone, commentsName}
+	
+	loaderModal.classList.toggle('show');
+
+    EmailJS.send(serviceId, emailTemplateId, templateParams)
+        .then(function(response) {
+		   console.log('SUCCESS!', response.status, response.text);
+		   loaderModal.classList.remove('show');
+		   successModal.classList.toggle('show');
+		   inputs.forEach(input => {
+			input.value = '';
+			input.blur();
+		});
+		   setTimeout(() => {
+			successModal.classList.remove('show');
+		   }, 3000);
+        }, function(error) {
+			successModal.classList.remove('show');
+			loaderModal.classList.toggle('show');
+		   	console.log('FAILED...', error);
+        });
+}
+
+form.onsubmit = function(e){
+	e.preventDefault();
+		const validatorResult = validator.checkAll(this);
+
+		validatorResult.fields.forEach((field, index) => {
+			if (!field.error) {
+				[inputs[1], inputs[0]][index].classList.remove('bad');
+			}
+		});
+
+	if (validatorResult.valid) {
+		sendEmail(nameField.value, telField.value, commentsField.value);
+	}
+};
+
+menuBtn.addEventListener('click', () => toggleModal());
+
+navLinkItems.forEach(link => {
+	link.addEventListener('click', (e) => toggleModal(e));
+});
+
+logo.addEventListener('click', () => {
+    window.scrollTo({
+        behavior: 'smooth',
+        top: 0
+    });
+});
+
+scrollBtn.addEventListener('click', () => {
+    const el = document.querySelector('.section-form');
+    const posY = el.getBoundingClientRect().top + window.pageYOffset - 100;
+    window.scrollTo({
+        top: posY,
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', () => {
+	isStickyNavbar = window.scrollY > 0;
+	navBar.classList.toggle('sticky', isStickyNavbar);
+
+	let showActionButtons = window.scrollY > document.querySelector('.section-header').clientHeight / 2;
+	actionButtons.classList.toggle('show', showActionButtons);
+});
+
+
+
+
+document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+    this.querySelector('.custom-select').classList.toggle('open');
+})
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
